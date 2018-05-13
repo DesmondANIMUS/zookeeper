b78c2076a32de3604d435599b2cb107ebd45744a
Gaurav Gogia <desmondmiles36@gmail.com>
2018-05-14 01:48:00 +0530

    typo corrected

--- src/containers/AnimalList.jsx
+++ src/containers/AnimalList.jsx
@@ -5,7 +5,7 @@ import React, { Component } from 'react';
 class AnimalList extends Component {    
     render() {     
         return (
-            <div className="grid-header">indian 
+            <div className="grid-header"> 
                 <h2 className="card no-selection light-text">Hello! Welcome to Animal List!!</h2>
                 {this.props.animals.map((animal, i) => <div className="card no-selection" key={i}>{animal.name}</div>)}
             </div>
