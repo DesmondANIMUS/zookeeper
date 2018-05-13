dc32d9f51a3a7ea3e2fc9e20e0772e5643b90d97
Gaurav Gogia <desmondmiles36@gmail.com>
2018-05-14 02:35:31 +0530

    ui update :)

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import AnimalList from './containers/AnimalList';
 
 const App = () => (
   <div className="grid">
-    <h1 className="grid-intro no-selection">ZoooKeeper</h1>
-    <AnimalList/>
+    <h2 className="grid-intro no-selection">ZoooKeeper</h2>
+    <AnimalList/>    
+    <div className="grid-right">
+      <h1 className="details no-selection">Details</h1>
+    </div> 
   </div>
 );
 
