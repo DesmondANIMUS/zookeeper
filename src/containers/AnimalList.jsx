710d73267f305f768ecd4641b1f499f042b7c3c4
DesmondANIMUS <desmondmiles36@gmail.com>
2018-05-28 02:16:38 +0530

    get animals from server and show them in list

--- src/containers/AnimalList.jsx
+++ src/containers/AnimalList.jsx
@@ -5,11 +5,18 @@ import Animal from '../components/Animal';
 import {getAnimals} from '../actions/actions';
 
 class AnimalList extends Component {        
+    componentWillMount() {
+        this.props.getAnimals();
+    }
     render() {                    
+        const renderAnimals = () => {
+            if (!this.props.animalList) return this.props.animals.map((animal, i) => <Animal key={i} animalInfo={animal}/>)
+            else return this.props.animalList.map((animal, i) => <Animal key={i} animalInfo={animal}/>)
+        }
         return (
             <div className="grid-animal-list">                 
                 <div className="btn card no-selection" onClick={() => this.props.getAnimals()}>Fetch Data</div>
-                {this.props.animals.map((animal, i) => <Animal key={i} animalInfo={animal}/>)}                
+                {renderAnimals()}                
                 <div className="btn card no-selection" onClick={() => this.props.getAnimals()}>Fetch Data</div>
             </div>
         );
@@ -18,7 +25,8 @@ class AnimalList extends Component {
 
 const mapStateToProps = (state) => {
     return {
-        animals: state.animals
+        animals: state.animals,
+        animalList: state.animalList
     }
 }
  
