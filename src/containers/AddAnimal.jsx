8e5b70e72f7f40ced084c273fa13a9d470369307
DesmondANIMUS <desmondmiles36@gmail.com>
2018-06-05 19:14:18 +0530

    addnimal form update

--- src/containers/AddAnimal.jsx
+++ src/containers/AddAnimal.jsx
@@ -2,11 +2,30 @@ import '../App.css';
 import React, { Component } from 'react';
 
 class AddAnimal extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            animal: {
+                animal_name: '',
+                species: ''
+            }
+        };
+    };
+    
+    onChange = (e) => this.setState({
+        animal: {...this.state.animal, [e.target.name]: e.target.value}
+    });    
+
     render() {
+        const {animal} = this.state;
         return (
             <div className="grid-two">
-                <input type="text" name="animalSpecies" id="animalSpecies" className="animal-add animal-input" placeholder="Animal Species"/>
-                <input type="text" name="animalName" id="animalName" placeholder="Animal Name" className="animal-input"/>                                        
+                <input type="text" name="species" id="species" 
+                        className="animal-add animal-input" placeholder="Animal Species" 
+                        onChange={this.onChange}  value={animal.species}/>
+                <input type="text" name="animal_name" id="animal_name" 
+                        placeholder="Animal Name" className="animal-input"
+                        onChange={this.onChange} value={animal.animal_name}/>                                        
                 <div className="btn-add no-selection">Add Animal</div>
             </div>
         );
